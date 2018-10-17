da66e23dcf1d21585a235cc06c5801667b080094
Federico Gimenez <federico.gimenez@gmail.com>
2018-10-17 18:53:18 +0200

    fix tests for v1 input format

--- src/docker/integration-tests/__tests__/analysis.test.js
+++ src/docker/integration-tests/__tests__/analysis.test.js
@@ -5,13 +5,53 @@ import submissionWithIssues from './submissionWithIssues';
 
 describe('/v1/analyses', () => {
   describe('Submit', () => {
+    let data;
+
+    beforeEach(() => {
+      data = {
+        contractName: 'TestMe',
+        abi: [
+          {
+            constant: false,
+            inputs: [
+              {
+                name: 'first_input',
+                type: 'uint256',
+              },
+            ],
+            name: 'lol',
+            outputs: [
+              {
+                name: '',
+                type: 'uint256',
+              },
+            ],
+            payable: false,
+            stateMutability: 'nonpayable',
+            type: 'function',
+          },
+        ],
+        bytecode: '00',
+        deployedBytecode: '00',
+        sourceMap: '25:78:1:-;;;;8:9:-1;5:2;;;30:1;27;20:12;5:2;25:78:1;;;;;;;',
+        deployedSourceMap: '25:78:1:-;;;;8:9:-1;5:2;;;30:1;27;20:12;5:2;25:78:1;;;;;;;',
+        sourceList: [
+          'basecontract.sol',
+          'maincontract.sol',
+        ],
+        sources: {
+          'basecontract.sol': '[... escaped source code ...]',
+          'maincontract.sol': '[... escaped source code ...]',
+        },
+        analysisMode: 'full',
+      };
+    });
+
     it('post analysis without authorization', async () => {
+      data.deployedBytecode = 'abcc';
       const res = await serverRequest
         .post('/v1/analyses')
-        .send({
-          type: 'bytecode',
-          contract: 'abcc',
-        })
+        .send(data)
         .expect(httpStatus.UNAUTHORIZED);
 
       expect(res.body.status).toBe(httpStatus.UNAUTHORIZED);
@@ -37,13 +77,11 @@ describe('/v1/analyses', () => {
       const {email, token} = await getValidCredential();
       await makeUserUnlimited(email);
 
+      data.deployedBytecode = 'abcc';
       let res = await serverRequest
         .post('/v1/analyses')
         .set('Authorization', `Bearer ${token}`)
-        .send({
-          type: 'bytecode',
-          contract: 'abcc',
-        })
+        .send(data)
         .expect(httpStatus.OK);
 
       expect(res.body).toHaveProperty('uuid');
@@ -64,41 +102,13 @@ describe('/v1/analyses', () => {
       const {email, token} = await getValidCredential();
       await makeUserUnlimited(email);
 
+      data.deployedBytecode = '01';
       const res = await serverRequest
         .post('/v1/analyses')
         .set('Authorization', `Bearer ${token}`)
-        .send({
-          type: 'bytecode',
-          contract: '01',
-        })
+        .send(data)
         .expect(httpStatus.OK);
       expect(res.body).toHaveProperty('uuid');
-    });
-
-    it('Submit multiple (no issues)', async () => {
-      const {email, token} = await getValidCredential();
-      await makeUserUnlimited(email);
-
-      let res = await serverRequest
-        .post('/v1/analyses')
-        .set('Authorization', `Bearer ${token}`)
-        .send({
-          type: 'bytecode',
-          contracts: ['abcc', '00', '11'],
-        })
-        .expect(httpStatus.OK);
-      expect(res.body).toHaveProperty('uuid');
-
-      await waitAnalysisStatus(res.body.uuid, 'Finished', token);
-
-      res = await serverRequest
-        .get(`/v1/analyses/${res.body.uuid}/issues`)
-        .set('Authorization', `Bearer ${token}`)
-        .expect(httpStatus.OK);
-
-      const foundIssues = res.body;
-      expect(foundIssues).toBeInstanceOf(Array);
-      expect(foundIssues.length).toBe(0);
     });
 
     it('issues', async () => {
